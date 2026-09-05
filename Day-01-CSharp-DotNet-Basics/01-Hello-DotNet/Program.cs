a1ba700b1f670c69f763c89d4da17c49596255d8
akash <akashgub21@gmail.com>
2026-09-05 23:28:28 +0600

    Add Day 1 Hello DotNet project

---
+++ Day-01-CSharp-DotNet-Basics/01-Hello-DotNet/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to the .NET Learning Journey!");
+Console.WriteLine("My name is Akassh");
+Console.WriteLine("I am learning C# and .NET"); 
+Console.WriteLine("This is Day 1 of my 30 Days .NET Challenge!"); 
