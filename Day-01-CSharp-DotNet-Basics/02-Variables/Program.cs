4ac399e3d82ace25b843d03a62d4305092deacb3
akash <akashgub21@gmail.com>
2026-09-05 23:48:58 +0600

    Add Day 1 variables practice

---
+++ Day-01-CSharp-DotNet-Basics/02-Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿string name = "Akash";
+int age = 25;
+double cgpa = 3.30;
+bool isStudent = true;
+
+string University = "Green University of Bangladesh"; 
+string Department = "Computer Science and Engineering"; 
+int GraduationYear = 2026;
+bool HasJob = false;
+
+
+Console.WriteLine("Student Information:");
+Console.WriteLine("--------------------");
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Age: " + age);
+Console.WriteLine("CGPA: " + cgpa);
+Console.WriteLine("Is Student: " + isStudent);
+
+Console.WriteLine("University: " + University);
+Console.WriteLine("Department: " + Department); 
+Console.WriteLine("Graduation Year: " + GraduationYear);
+Console.WriteLine("Has Job: " + HasJob);
+
