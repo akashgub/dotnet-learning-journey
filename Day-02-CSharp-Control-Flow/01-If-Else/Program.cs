3c918033222ffa0deba887c96025734f7d3dda4b
akash <akashgub21@gmail.com>
2026-09-06 13:51:19 +0600

    Add Day 2 if else practice

---
+++ Day-02-CSharp-Control-Flow/01-If-Else/Program.cs
@@ -0,0 +1,11 @@
+﻿int age = 22;
+int Experience = 1;
+
+if (age >= 18 && Experience >= 1)
+{
+    Console.WriteLine("You are eligible for the job.");
+}
+else
+{
+    Console.WriteLine("You are not eligible for the job.");
+}
