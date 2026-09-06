3cce85292713549809ca4cb5de88dc9362cf2a67
akash <akashgub21@gmail.com>
2026-09-06 10:30:50 +0600

    Add Day 1 var practice

---
+++ Day-01-CSharp-DotNet-Basics/04-Var/Program.cs
@@ -0,0 +1,21 @@
+﻿
+var ProductName = "Laptop"; // string ProductName = "Laptop";
+var ProductID = 101; // int ProductID = 101;
+var ProductPrice = 1500.75; // double ProductPrice = 1500.
+var ProductQuantity = 10; // int ProductQuantity = 10;
+var Rating = 4.5f; // float Rating = 4.5f; 
+var CategoryInitial = 'E'; // char CategoryInitial = 'E';
+var IsAvailable = true; // bool IsAvailable = true;
+
+Console.WriteLine("Product Information:");
+Console.WriteLine("------------------");
+
+Console.WriteLine("Product Name: " + ProductName);
+Console.WriteLine("Product ID: " + ProductID);
+Console.WriteLine("Product Price: " + ProductPrice);
+Console.WriteLine("Product Quantity: " + ProductQuantity);
+Console.WriteLine("Rating: " + Rating);
+Console.WriteLine("Category Initial: " + CategoryInitial);
+Console.WriteLine("Is Available: " + IsAvailable);
+
+
