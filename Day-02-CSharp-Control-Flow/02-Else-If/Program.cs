a947cff121adb1ee91f311d51fdec57716fbce17
akash <akashgub21@gmail.com>
2026-09-06 14:08:41 +0600

    Add Day 2 else if grade checker

---
+++ Day-02-CSharp-Control-Flow/02-Else-If/Program.cs
@@ -0,0 +1,27 @@
+﻿string studentName = "Akash";
+int marks = 85;
+
+
+Console.WriteLine("Student Results");
+Console.WriteLine("----------------");
+Console.WriteLine($"Student Name: {studentName}");
+Console.WriteLine($"Marks: {marks}");
+
+if (marks >= 90){
+    Console.WriteLine("Grade: A+");
+}
+else if (marks >= 80){
+    Console.WriteLine("Grade: A");
+}
+else if (marks >= 70){
+    Console.WriteLine("Grade: B");
+}
+else if (marks >= 60){
+    Console.WriteLine("Grade: C");
+}
+else if (marks >= 40){
+    Console.WriteLine("Grade: D");
+}
+else{
+    Console.WriteLine("Grade: F");
+}
