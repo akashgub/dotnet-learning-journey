e5b1849736f5dac6c02a65c380ab2767c2ce81a5
akash <akashgub21@gmail.com>
2026-09-06 10:06:19 +0600

    Add Day 1 data types practice

---
+++ Day-01-CSharp-DotNet-Basics/03-Data-Types/Program.cs
@@ -0,0 +1,20 @@
+﻿string EmployeeName = "Akash";
+int EmployeeID = 101;
+decimal Salary = 50000.50;
+float Experience = 3.5f;
+decimal performanceRating = 4.5m;
+char Department = 'C';
+bool IsFullTime = true;
+string companyName = "Tech Solutions Ltd.";
+
+Console.WriteLine("Employee Information:");
+Console.WriteLine("----------------");
+
+Console.WriteLine("Employee Name: " + EmployeeName);
+Console.WriteLine("Employee ID: " + EmployeeID);
+Console.WriteLine("Salary: " + Salary);
+Console.WriteLine("Experience: " + Experience);
+Console.WriteLine("Performance Rating: " + performanceRating);
+Console.WriteLine("Department: " + Department);
+Console.WriteLine("Is Full-Time: " + IsFullTime);
+Console.WriteLine("Company Name: " + companyName);
