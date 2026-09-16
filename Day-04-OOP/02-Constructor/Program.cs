e2e8f9a4de21e2f85f32caf776f053e012ec449a
akash <akashgub21@gmail.com>
2026-09-16 23:22:03 +0600

    Complete Day 4 OOP class object constructor and methods

---
+++ Day-04-OOP/02-Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿class Person
+{
+    public string name;
+    public int age;
+
+}
+
+class Test 
+{
+    public static void Main(string[] args)
+    {
+        Person p1 = new Person();
+        p1.name = "Akash";
+        p1.age = 24;
+        Console.WriteLine($"Name: {p1.name}, Age: {p1.age}\n"); 
+
+        Person p2 = new Person();
+        p2.name = "Karim";
+        p2.age = 30;
+        Console.WriteLine($"Name: {p2.name}, Age: {p2.age}\n"); 
+
+        
+    }
+}
---
+++ Day-04-OOP/03-Method-Class-Member/Program.cs
@@ -0,0 +1,31 @@
+﻿class Person
+{
+    public string name;
+    public int age;
+
+    public void SetValue(string n, int a){
+        name = n;
+        age = a;
+    }
+
+    public void DisplayInfo(){
+        Console.WriteLine($"Name: {name}, Age: {age}\n"); 
+
+}
+
+}
+
+class Test 
+{
+    public static void Main(string[] args)
+    {
+        Person p1 = new Person();
+        p1.SetValue("Akash", 25);
+        p1.DisplayInfo();
+
+        Person p2 = new Person();
+        p2.SetValue("Karim", 30);
+        p2.DisplayInfo();
+        
+    }
+}
