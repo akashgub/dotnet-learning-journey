7abc1de739895429ebce011012a20bc7a0ec713b
akash <akashgub21@gmail.com>
2026-09-17 23:48:35 +0600

    Complete Day 4 Inheritance

---
+++ Day-04-OOP/05-Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿class Vehicle
+{
+    public string Brand;
+
+    public void Start()
+    {
+        Console.WriteLine("Vehicle is starting.");
+    }
+}
+
+class Car : Vehicle
+{
+    public void Drive()
+    {
+        Console.WriteLine("Car is driving.");
+    }
+}
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+
+        car.Brand = "Toyota";
+
+        Console.WriteLine(car.Brand);
+        car.Start();
+        car.Drive();
+    }
+}
