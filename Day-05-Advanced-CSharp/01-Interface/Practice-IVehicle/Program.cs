dd2f9ad376211af1b1cb11629098c737f4bd2af2
akash <akashgub21@gmail.com>
2026-09-18 23:57:57 +0600

    Complete Day 5 Interface practice

---
+++ Day-05-Advanced-CSharp/01-Interface/Practice-IVehicle/Program.cs
@@ -0,0 +1,33 @@
+﻿
+interface IVehicle
+{
+    void Start();
+}
+
+class Car : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Car is starting.");
+    }
+}
+
+class Bike : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Bike is starting.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IVehicle car = new Car();
+        car.Start();
+
+        IVehicle bike = new Bike();
+        bike.Start();
+    }
+}
