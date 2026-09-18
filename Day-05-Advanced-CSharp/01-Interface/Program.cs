fac59fd104626c35a27b42be688f0edb5b4196a1
akash <akashgub21@gmail.com>
2026-09-18 23:53:33 +0600

    Complete Day 5 Interface concept

---
+++ Day-05-Advanced-CSharp/01-Interface/Program.cs
@@ -0,0 +1,31 @@
+﻿interface IPayment
+{
+    void Pay();
+}
+
+class BkashPayment : IPayment
+{
+    public void Pay()
+    {
+        Console.WriteLine("Payment completed using bKash");
+    }
+}
+class CardPayment : IPayment
+{
+    public void Pay()
+    {
+        Console.WriteLine("Payment completed using Card");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        IPayment payment1 = new BkashPayment();
+        payment1.Pay();
+
+        IPayment payment2 = new CardPayment();
+        payment2.Pay();
+
+    }
+}
