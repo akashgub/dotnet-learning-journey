88998ece4c05e8d0b360e5c2f6ae8e66197cb768
akash <akashgub21@gmail.com>
2026-09-19 00:05:07 +0600

    Complete Day 5 Abstract Class concept

---
+++ Day-05-Advanced-CSharp/02-Abstract-Class/Program.cs
@@ -0,0 +1,42 @@
+﻿
+abstract class Vehicle
+{
+    public abstract void Start();
+
+    public void Stop()
+    {
+        Console.WriteLine("Vehicle is stopping.");
+    }
+}
+
+class Car : Vehicle
+{
+    public override void Start()
+    {
+        Console.WriteLine("Car is starting.");
+    }
+}
+
+class Bike : Vehicle
+{
+    public override void Start()
+    {
+        Console.WriteLine("Bike is starting.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Vehicle car = new Car();
+        car.Start();
+        car.Stop();
+
+        Console.WriteLine();
+
+        Vehicle bike = new Bike();
+        bike.Start();
+        bike.Stop();
+    }
+}
