3153e4957605c1f8a777850b46dd5a3a36bb78c1
akash <akashgub21@gmail.com>
2026-09-22 23:33:03 +0600

    Complete Day 4 Interace

---
+++ Day-04-OOP/08-Interface/Program.cs
@@ -0,0 +1,30 @@
+﻿interface IVehicle
+{
+    void Start();
+}
+
+class Car : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Car is starting.");
+    }
+}
+class Bike : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Bike is starting.");
+    }
+}
+class Test
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.Start();
+
+        Bike bike = new Bike();
+        bike.Start();
+    }
+}
