38e103ec483776f31499927ed007099034699ec9
akash <akashgub21@gmail.com>
2026-09-22 19:29:07 +0600

    Complete Day 4 Abstraction

---
+++ Day-04-OOP/07-Abstraction/Program.cs
@@ -0,0 +1,38 @@
+﻿abstract class Payment
+{
+    public abstract void Pay();
+
+    public void ShowMessage(){
+        Console.WriteLine("Payment processing...");
+    }
+}
+
+class BkashPayment : Payment
+{
+    public override void Pay(){
+        Console.WriteLine("Paid using Bkash.\n");
+    }
+}
+
+class CardPayment : Payment
+{
+    public override void Pay(){
+        Console.WriteLine("Paid using Card.");
+    }
+}
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        Payment payment1 = new BkashPayment();
+
+        payment1.ShowMessage();
+        payment1.Pay();
+
+        Payment payment2 = new CardPayment();
+
+        payment2.ShowMessage();
+        payment2.Pay();
+    }
+}
